6b597655c61e06016261cf81dd0478ed0d8935e8
Maurice Rethmann <maurice.rethmann@hs-osnabrueck.de>
2021-05-01 14:05:59 +0200

    Initial Commit

---
+++ Gießformkonfigurator.WPF/main/castingMoldManagement/Ring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gießformkonfigurator.WPF.main.castingMoldManagement
+{
+    public class Ring : Component
+    {
+    }
+}
---
+++ Gießformkonfigurator.WindowsForms/main/castingMoldManagement/Ring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gießformkonfigurator.WindowsForms.main.castingMoldManagement
+{
+    public class Ring : Component
+    {
+    }
+}
